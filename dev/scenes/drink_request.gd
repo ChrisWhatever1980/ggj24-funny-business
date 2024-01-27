45dc0c973eb5ab5836578063a566048d3baa6854
Markus Dittmann <dmarku@posteo.de>
2024-01-27 17:22:25 +0100

    Remove request from drink's fulfillable requests if it times out

--- dev/scenes/drink_request.gd
+++ dev/scenes/drink_request.gd
@@ -3,6 +3,7 @@ extends Area2D
 signal fulfilled(tip: bool)
 signal timed_out
 
+var current_drink = null
 var tip = true
 
 # Called when the node enters the scene tree for the first time.
@@ -17,10 +18,12 @@ func _process(delta):
 
 func _on_area_entered(area: Area2D):
 	if area.is_in_group("Drinks"):
+		current_drink = area
 		area.add_request(self)
 		
 func _on_area_exited(area: Area2D):
 	if area.is_in_group("Drinks"):
+		current_drink = null
 		area.remove_request(self)
 		
 func _on_failure_timer_timeout():
@@ -43,4 +46,6 @@ func fulfill():
 	disappear()
 
 func disappear():
+	if current_drink != null:
+		current_drink.remove_request(self)
 	queue_free()
