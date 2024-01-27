8d6cf15fbad52084eb10494a0d369f666a5adbfa
Markus Dittmann <dmarku@posteo.de>
2024-01-27 16:44:19 +0100

    Add timeout for drink requests, display progress bar

--- dev/scenes/drink_request.gd
+++ dev/scenes/drink_request.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 signal fulfilled
+signal timed_out
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,7 +10,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	$FailureProgressBar.value = $FailureTimer.time_left / $FailureTimer.wait_time
 
 
 func _on_area_entered(area: Area2D):
@@ -19,6 +20,10 @@ func _on_area_entered(area: Area2D):
 func _on_area_exited(area: Area2D):
 	if area.is_in_group("Drinks"):
 		area.remove_request(self)
+		
+func _on_failure_timer_timeout():
+	timed_out.emit()
+	disappear()
 
 func highlight_fulfillable(fulfillable: bool):
 	if fulfillable:
@@ -27,5 +32,9 @@ func highlight_fulfillable(fulfillable: bool):
 		$Sprite2D.modulate = Color.WHITE
 
 func fulfill():
+	$FailureTimer.stop()
 	fulfilled.emit()
+	disappear()
+
+func disappear():
 	queue_free()
