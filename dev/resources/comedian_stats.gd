cf39c3778a2e8109ee5aa02c1b2c5dd285f72171
jan <jan.lachnit@breakpoint.one>
2024-01-27 12:35:17 +0100

    Added Comedians

--- dev/resources/comedian_stats.gd
+++ dev/resources/comedian_stats.gd
@@ -1,14 +1,14 @@
 extends Resource
 
-@export var name: String
-@export var cost: int
-@export var fame: int
-@export var comedy: int
-@export var bomb_chance: int
-@export var hit_chance: int
-@export var availability: int
-@export var club_fame_requirement: int
-@export var endurance: int
+@export var name: String = "Comedian"
+@export var cost: int = 10
+@export var fame: int = 10
+@export var comedy: int = 10
+@export var bomb_chance: int = 10
+@export var hit_chance: int = 10
+@export var availability: int = 10
+@export var club_fame_requirement: int = 0
+@export var endurance: int = 10
 
 func _init():
 	pass
